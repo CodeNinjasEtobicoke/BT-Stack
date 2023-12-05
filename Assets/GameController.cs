c4f6c7150f1f0ebeb974dd11739f57735c25b463
CodeNinjasEtobicoke <codeninjasetobicoke@outlook.com>
2023-12-05 17:51:47 -0500

    hai

---
+++ Assets/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameController : MonoBehaviour
+{
+    [Header("CO")]
+    public GameObject currentCube;
+    [Header("LCO")]
+    public GameObject lastCube;
+    [Header("TO")]
+    public Text text;
+    [Header("CL")]
+    public int Level;
+    [Header("Bool")]
+    public bool Done;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void NewBlock()
+    {    
+        if(lastCube != null)
+        {
+            //currentCube.transform.position = new Vector3(Mathf.Round(currentCube.transform.position.x)),
+        //currentCube.transform.position.y,
+               // Mathf.Round(currentCube.transform.position.z));
+           //currentCube
+        }
+    }
+}
