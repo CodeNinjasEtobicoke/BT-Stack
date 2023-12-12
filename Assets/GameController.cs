a9fe6c431320cba169d96da84317934bf381a47a
CodeNinjasEtobicoke <107214218+CodeNinjasEtobicoke@users.noreply.github.com>
2023-12-12 17:43:43 -0500

    hi

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
@@ -31,7 +32,7 @@ public class GameController : MonoBehaviour
         }
         var time = Mathf.Abs(Time.realtimeSinceStartup % 2f - 1f);
         var pos1 = lastCube.transform.position + Vector3.up * 10f;
-        var pos2 = pos1 + ((Level % 2 == 0)) ? Vector3.left : Vector3.forward) * 120;
+        var pos2 = pos1 + ((Level % 2 == 0) ? Vector3.left : Vector3.forward) * 120;
         if(Level % 2 == 0)
         {
             currentCube.transform.position = Vector3.Lerp(pos2, pos1, time);
@@ -49,7 +50,8 @@ public class GameController : MonoBehaviour
         if (lastCube != null)
         {
             currentCube.transform.position = new Vector3(Mathf.Round(currentCube.transform.position.x),
-            currentCube.transform.position.y, Mathf.Round(currentCube.transform.position.z));
+            currentCube.transform.position.y,
+            Mathf.Round(currentCube.transform.position.z));
 
             currentCube.transform.localScale = new Vector3(lastCube.transform.localScale.x - Mathf.Abs(currentCube.transform.position.x - lastCube.transform.position.x),
                 lastCube.transform.localScale.y,
@@ -74,11 +76,11 @@ public class GameController : MonoBehaviour
         currentCube.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.HSVToRGB((Level / 100f) % 1f, 1f, 1f));
         Level++;
         Camera.main.transform.position = currentCube.transform.position + new Vector3(100, 100, 100);
-        Camera.main.transform.LookAt(currentCube.transform.position + new Vector3(100, 100, 100);
+        Camera.main.transform.LookAt(currentCube.transform.position);
     }
     IEnumerator X()
     {
         yield return new WaitForSeconds(3f);
-        //SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("SampleScene");
     }  
 }
